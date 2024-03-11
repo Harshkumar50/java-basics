f905430fdf67bc1370f6f1a703ff84801dc317a3
Harsh kumar <115373712+Harshkumar50@users.noreply.github.com>
2024-03-11 21:52:44 +0530

    Create ndd.java

---
+++ ndd.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class lec15bit {
+public static void main(String[] args) {
+int n = 5;
+int pos = 2;
+int bitmask = 1 << pos;
+if ((bitmask & n) == 0) {
+System.out.println("Bit is 0");
+} else {
+System.out.println("Bit is 1");
+}
+}
+}
