25141e4f2a0ecc73546f163151da914191b29a5e
Harsh kumar <115373712+Harshkumar50@users.noreply.github.com>
2024-03-21 15:39:57 +0530

    Create sort.java

---
+++ sort.java
@@ -0,0 +1,17 @@
+import java.util.* ;
+import java.io.*; 
+
+public class Solution {
+
+    static int largestElement(int[] arr, int n) {
+        // Write your code here.
+        int temp=0;
+        int i =0;
+        Arrays.sort(arr);
+        for(;i<=arr.length;i++){
+           return arr[arr.length-1];
+        }
+        return arr.length;
+    }
+}
+
