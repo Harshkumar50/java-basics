5fcc6876ebfa8b9cf1d1f834dee490ff0a45cabb
Harsh kumar <115373712+Harshkumar50@users.noreply.github.com>
2024-04-07 22:08:17 +0530

    Create ninja.java

---
+++ ninja.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class hw4 {
+    public static Double Ninja(Double rad) {
+        return 2 * 3.14 * rad;
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        Double r = sc.nextDouble();
+        System.out.println(Ninja(rad));
+
+    }
+}
